98352036fca3bbbb5bfbaffeedc95764854a49bd
moisesduartem <moisesduartem@gmail.com>
2020-09-25 14:16:41 -0300

    organiza buttons e poe date

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,11 +3,14 @@
     <div class="container rounded p-0 shadow-sm">
       <div id="add-task" class="list-group-item bg-light">
         <input type="text" class="form-control" placeholder="Adicionar Tarefa">
-        
+        <Button>+</Button>
       </div>
       <div class="">
-        <div class="list-group-item bg-light text-left d-flex align-items-center  justify-content-between" v-for="(task) in taskList" v-bind:key="task">
-          {{task}}
+        <div class="list-group-item bg-light text-left d-flex align-items-center  justify-content-between" v-for="(task) in taskList" v-bind:key="task.id">
+          {{task.title}}
+          <small>
+            {{ new Date(task.date).toLocaleString('pt-br') }}
+          </small>
           <Button color='danger' circle=true>
             <TrashIcon />
           </Button>
@@ -24,7 +27,10 @@
     data() {
       return {
         name: 'List',
-        taskList: ['Lavar a louça', 'Varrer a casa']      
+        taskList: [
+        {title: 'Lavar a louça', date: '2020-06-25 13:00:00'}, 
+        {title: 'Varrer a casa', date: '2020-06-20 08:00:00'}
+        ]      
       }
     },
     components: {
@@ -35,7 +41,6 @@
 </script>
 
 <style scoped>
-
 #add-task {
   display: flex;
 }
