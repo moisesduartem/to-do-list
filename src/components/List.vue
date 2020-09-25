ce46d362c71c4f53cd6634d548d6bfbb43fb90ea
moisesduartem <moisesduartem@gmail.com>
2020-09-25 12:58:11 -0300

    inicia logica de listagem

---
+++ src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="list">
+    <div v-for="(item, i) in thingsToDo" v-bind:key="item">
+      {{i + 1}}) {{item}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        name: 'List',
+        thingsToDo: ['Lavar a louça', 'Varrer a casa']      
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
